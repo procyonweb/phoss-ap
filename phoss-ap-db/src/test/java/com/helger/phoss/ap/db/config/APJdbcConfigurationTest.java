ffa32b3bb416e55a9a2cf300217bdb5016d217af
Philip Helger <philip@helger.com>
2026-03-01 17:49:50 +0100

    Updated

--- phoss-ap-db/src/test/java/com/helger/phoss/ap/db/config/APJdbcConfigurationTest.java
+++ phoss-ap-db/src/test/java/com/helger/phoss/ap/db/config/APJdbcConfigurationTest.java
@@ -18,7 +18,6 @@ package com.helger.phoss.ap.db.config;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
