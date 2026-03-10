885f753c4cf713f756fe647743db5cd1ab2afd82
Philip Helger <philip@helger.com>
2026-03-10 20:48:04 +0100

    Added comment

--- phoss-ap-webapp/src/main/java/com/helger/phoss/ap/webapp/PhossAPApplication.java
+++ phoss-ap-webapp/src/main/java/com/helger/phoss/ap/webapp/PhossAPApplication.java
@@ -20,6 +20,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+// Run with "--spring.profiles.active=private"
 @SpringBootApplication
 @EnableScheduling
 public class PhossAPApplication
